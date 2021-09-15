a18205abfae49be7ec48576a7e36e0b6a5ea3417
João Paulo <joaopdejesus79@gmail.com>
2021-09-15 17:02:10 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AreaTrianguloRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Digite a medida da altura do seu triângulo retângulo: ");
+            Console.ResetColor();
+            string alturaDigitada = Console.ReadLine();
+            double altura = Convert.ToDouble(alturaDigitada);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Digite a medida da base do seu triângulo retângulo: ");
+            Console.ResetColor();
+            string baseDigitada = Console.ReadLine();
+            double baseTotal = Convert.ToDouble(baseDigitada);
+            double area = baseTotal * altura / 2;
+            Console.WriteLine();
+            Console.WriteLine($"Base..: {baseTotal}");
+            Console.WriteLine($"Altura: {altura}");
+            Console.WriteLine();
+            Console.WriteLine($"Área..: {area}");
+            Console.WriteLine();
+        }
+    }
+}
